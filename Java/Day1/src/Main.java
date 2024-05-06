c54d84dcaf1049e6fa4c8938923df86da8ac3c06
talha.rizwan <talha.rizwan7170@hotmail.com>
2024-05-06 20:48:56 +0500

    refactored to new file convention that everyone should follow

--- Java/Day1/src/Main.java
+++ Java/Day1/src/Main.java
@@ -2,34 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        String[] strs = {"eat","tea","tan","ate","nat","bat"};
-
-        Map<String, List> hashMap = new HashMap<>();
-
-        for (int i = 0; i < strs.length; i++) {
-            char[] chars = strs[i].toCharArray();
-            Arrays.sort(chars);
-            String sortedStr = new String(chars);
-
-            if(!hashMap.containsKey(sortedStr)){
-                List entires = new ArrayList<>();
-                entires.add(strs[i]);
-                hashMap.put(sortedStr,entires);
-            }
-            else{
-                List entries = hashMap.get(sortedStr);
-                entries.add(strs[i]);
-                hashMap.put(sortedStr,entries);
-            }
-        }
-
-        List<List<String>> finalList = new ArrayList<>();
-        for (Map.Entry<String, List> entry : hashMap.entrySet()) {
-            finalList.add(entry.getValue());
-        }
-
-        System.out.println(finalList);
+        GroupAnagrams.solve();
 
     }
 }
