ae3f13b05cf7fe9ed3a7cb93c948b6d474e4acb0
talha.rizwan <talha.rizwan7170@hotmail.com>
2025-02-12 21:59:37 +0500

    [Practice] Binary search tree in,pre,post order traversals

--- Java/Practice/src/Main.java
+++ Java/Practice/src/Main.java
@@ -1,8 +1,30 @@
+import TreePractice.BinaryTree;
+
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         MinHeapInternal minHeap = new MinHeapInternal();
         minHeap.minHeapPracticeTopKElements(List.of(1,7,5,4,9,2), 3);
+
+        System.out.println("Binary Search Tree Testing");
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.insert(40);
+        binaryTree.insert(30);
+        binaryTree.insert(50);
+        binaryTree.insert(25);
+        binaryTree.insert(35);
+        binaryTree.insert(15);
+        binaryTree.insert(28);
+        binaryTree.insert(45);
+        binaryTree.insert(60);
+        binaryTree.insert(55);
+        binaryTree.insert(70);
+
+        binaryTree.inorder();
+        System.out.println();
+        binaryTree.preorder();
+        System.out.println();
+        binaryTree.postorder();
     }
 }
--- Java/Practice/src/TreePractice/BinaryTree.java
+++ Java/Practice/src/TreePractice/BinaryTree.java
@@ -1,9 +1,12 @@
 package TreePractice;
 
 class Node {
-
     int data;
     Node left, right;
+
+    Node(int data) {
+        this.data = data;
+    }
 
 }
 
@@ -11,6 +14,57 @@ public class BinaryTree {
 
     Node root;
 
-    
+    public void insert(int data) {
+        this.root = insertRec(root, data);
+    }
+
+    private Node insertRec(Node root, int data) {
+        if(root == null) {
+            root = new Node(data);
+        }
+        else if(data < root.data) {
+            root.left = insertRec(root.left, data);
+        }
+        else if(data > root.data) {
+            root.right = insertRec(root.right, data);
+        }
+        return root;
+    }
+
+    public void inorder() {
+        inorderRec(root);
+    }
+
+    private void inorderRec(Node root) {
+        if(root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    public void preorder() {
+        preorderRec(root);
+    }
+
+    private void preorderRec(Node root) {
+        if(root != null) {
+            System.out.print(root.data + " ");
+            preorderRec(root.left);
+            preorderRec(root.right);
+        }
+    }
+
+    public void postorder() {
+        postorderRec(root);
+    }
+
+    private void postorderRec(Node root) {
+        if (root != null) {
+            postorderRec(root.left);
+            postorderRec(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
 
 }
